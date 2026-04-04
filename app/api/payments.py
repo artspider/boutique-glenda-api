1c534b552b4f7d315613f7b0b4a45764b162b783
Arturo Rodriguez <arturo.itiguala@gmail.com>
2026-04-03 18:55:07 -0600

    verify payments endpoint already protected with jwt

--- app/api/payments.py
+++ app/api/payments.py
@@ -4,11 +4,17 @@ from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.schemas.payment_create import PaymentCreate
 from app.services.payment_service import PaymentService
+from app.models.user import User
+from app.core.security import get_current_user
 
 router = APIRouter()
 
 
 @router.post("/payments")
-def register_payment(payload: PaymentCreate, db: Session = Depends(get_db)):
+def register_payment(
+    payload: PaymentCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     service = PaymentService(db)
     return service.register_payment(payload)
