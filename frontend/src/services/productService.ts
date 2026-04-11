b70f6133b04da022168cde8a5cf94a5a8babcc81
Arturo Rodriguez <arturo.itiguala@gmail.com>
2026-04-10 19:26:15 -0600

    fix frontend api base url for production and enable nginx proxy

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,6 +1,7 @@
 import { getAuthorizationHeader, handleUnauthorized } from './authService';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+//const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
 
 export type Product = {
   id: number;
